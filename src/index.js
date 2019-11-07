69559557c0af5cd584380f66c8859f862dbe65a6
hehu <316031602@qq.com>
2019-11-07 15:35:59 +0800

    update

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className='app'>
-        <div className='app-desc'>构建自己的React脚手架</div>
+        <div className='app-desc'>使用webpack构建React脚手架</div>
       </div>
     )
   }
