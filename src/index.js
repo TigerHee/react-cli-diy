3907ff2a89b35e2de2a8adb70a4c5bfcc07c1c31
hehu <316031602@qq.com>
2019-11-08 09:50:07 +0800

    indexjs更新

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 import './index.less'
-console.log('ENV === ', ENV)
+
 class App extends Component {
   render() {
     return (
